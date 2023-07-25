c4353129722e09841a52eaa2a5e596a139ac3ad7
carolinerobbins <caroline.tawney@gmail.com>
2023-07-25 16:16:21 -0700

    fixed sign up document id vs uuid

--- App.js
+++ App.js
@@ -12,14 +12,12 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
   const auth = getAuth();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setUser(user);
-      setLoading(false);
     });
   }, [auth]);
 
