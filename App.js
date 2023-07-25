e38f5301213e06a2ee2ddb975e5ad1803b92e2f6
carolinerobbins <caroline.tawney@gmail.com>
2023-07-24 20:37:44 -0700

    configured firebase and set up login and signup screen

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { Switch, Text, View } from "react-native";
-import { useColorScheme } from "nativewind";
+import React, {useState} from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import MainTabs from './src/navigation/BottomTabNav';
 import NavStack from './src/navigation/NavStack';
+import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen'
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
   return (
     <NavigationContainer>
     <Stack.Navigator headerShown="false">
-      <Stack.Screen name="BucketList" component={MainTabs} />
+    { user ? (
+          <Stack.Screen name="BucketList" component={MainTabs} />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </>
+        )}
     </Stack.Navigator>
   </NavigationContainer>
   );
