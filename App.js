944bdee70e336665bf4042b5a4d02a46d88f7a54
carolinerobbins <caroline.tawney@gmail.com>
2023-07-25 10:50:33 -0700

    completed auth process

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import MainTabs from './src/navigation/BottomTabNav';
 import NavStack from './src/navigation/NavStack';
 import LoginScreen from './src/screens/LoginScreen';
-import SignUpScreen from './src/screens/SignUpScreen'
+import SignUpScreen from './src/screens/SignUpScreen';
+import * as firebaseConfig from './firebaseConfig';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+  }, [auth]);
 
   return (
     <NavigationContainer>
     <Stack.Navigator headerShown="false">
     { user ? (
-          <Stack.Screen name="BucketList" component={MainTabs} />
+          <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }}/>
         ) : (
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
