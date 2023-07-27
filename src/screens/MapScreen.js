73b4077d57400921abb498582012d6a06c0e964c
carolinerobbins <caroline.tawney@gmail.com>
2023-07-27 09:37:34 -0700

    added markers to map

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -15,49 +15,90 @@ import {
 import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = ({ navigation }) => {
-  const [mapRegion, setmapRegion] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
-  // const [added, setAdded] = useState([]);
-  // const [visited, setVisited] = useState([]);
-  // const db = getFirestore();
+  const [added, setAdded] = useState([]);
+  const [visited, setVisited] = useState([]);
+  const db = getFirestore();
 
-  // const getMarkers = async (userId) => {
-  //   try {
-  //     const userRef = doc(db, "users", userId);
-  //     const userDoc = await getDoc(userRef);
+  const getMarkers = async (userId) => {
+    try {
+      const userRef = doc(db, "users", userId);
+      const userDoc = await getDoc(userRef);
 
-  //     const addedDestinations = userDoc.data().added || [];
-  //     const visitedDestinations = userDoc.data().visited || [];
+      const addedDestinations = userDoc.data().added || [];
+      const visitedDestinations = userDoc.data().visited || [];
 
-  //     return [addedDestinations, visitedDestinations];
-  //   } catch (error) {
-  //     console.error("Error fetching destinations:", error);
-  //     return [];
-  //   }
-  // };
+      const destinationsRef = collection(db, "destinations");
 
-  // useEffect(() => {
-  //   getMarkers("NStFWjztTjbeSivO95euaNBlrdO2")
-  //     .then((data) => {
-  //       setAdded(data[0]);
-  //       setVisited(data[1]);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error:", error);
-  //     });
-  // }, []);
+      const addedQuery = query(
+        destinationsRef,
+        where("__name__", "in", addedDestinations)
+      );
+
+      const addedSnapshot = await getDocs(addedQuery);
+      const addedMarkers = addedSnapshot.docs.map((doc) => doc.data());
+
+      const visitedQuery = query(
+        destinationsRef,
+        where("__name__", "in", visitedDestinations)
+      );
+      const visitedSnapshot = await getDocs(visitedQuery);
+      const visitedMarkers = visitedSnapshot.docs.map((doc) => doc.data());
+
+      return [addedMarkers, visitedMarkers];
+    } catch (error) {
+      console.error("Error fetching destinations:", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    getMarkers("NStFWjztTjbeSivO95euaNBlrdO2")
+      .then((data) => {
+        setAdded(data[0]);
+        setVisited(data[1]);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
   return (
-      <SafeAreaView className="flex-1">
-        <Text>This is the map view.</Text>
+    <SafeAreaView className="flex-1 justify-center  dark:bg-slate-800">
+      <View className='items-center'>
+      <Text className="text-xl dark:text-slate-50">My BucketList</Text>
+      <Text className="text-md dark:text-slate-50">Green: I've been here! </Text>
+      <Text className="text-md dark:text-slate-50">Blue: I want to go here!</Text>
+      </View>
       <MapView
-        style={{ alignSelf: 'stretch', height: '70%' }}
-        region={mapRegion}
-      />
+        style={{ flex: 1, height: '80%', margin: 10 }}
+        initialRegion={{
+          latitude: 0,
+          longitude: 0,
+          latitudeDelta: 180,
+          longitudeDelta: 180,
+        }}
+      >
+        {added.map((destination) => (
+          <Marker
+            key={destination.name}
+            coordinate={{
+              latitude: destination.latitude,
+              longitude: destination.longitude,
+            }}
+            pinColor={"blue"}
+          />
+        ))}
+        {visited.map((destination) => (
+          <Marker
+            key={destination.name}
+            coordinate={{
+              latitude: destination.latitude,
+              longitude: destination.longitude,
+            }}
+            pinColor={"green"}
+          />
+        ))}
+      </MapView>
     </SafeAreaView>
   );
 };
